251e082940ac59507c7e33c2d2a448d7d33a5f7d
JEIN <kienaikizna@gmail.com>
2014-05-02 13:58:50 +0900

    telephone test

--- js/tel.js
+++ js/tel.js
@@ -2,6 +2,29 @@
  * Created by NHNent on 2014-05-02.
  */
 function tel(id) {
-    var root = document.getElementById(id);
-    root.innerHTML = 'root';
+
+}
+
+function Tel() {
+    this.input = null;
+    this.list = {send:null, receive: null};
+}
+
+function NumberManager() {
+    function inputNumber(number) {
+        this.input += number;
+    }
+    function resetNumber() {
+        this.input = null;
+    }
+    function clearOneNumber() {
+        this.input = this.input.slice(0, -1);
+    }
+}
+
+function SendManager() {
+    //var regExp = /^(01[016789]{1}|02|0[3-9]{1}[0-9]{1})-?[0-9]{3,4}-?[0-9]{4}$/;
+    function checkNumber(number) {
+        //regExp.test(numbe)
+    }
 }
