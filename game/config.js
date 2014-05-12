acb1d72e098444662bb99d1e2fa742d6234daaee
JEIN <kienaikizna@gmail.com>
2014-05-12 19:10:15 +0900

    게임 커밋

---
+++ game/config.js
@@ -0,0 +1,9 @@
+/**
+ * Created by NHNent on 2014-05-12.
+ */
+var config = config || {};
+config.static = {
+    width: 320,
+    height: 480,
+    grade: 0
+};
--- game/js/app.js
+++ game/js/app.js
@@ -1,3 +1,28 @@
 /**
  * Created by NHNent on 2014-05-12.
  */
+/*function resize(){
+    if (screen.width != globalVars.width || screen.height != globalVars.height) {
+        var clientWidth = document.body.clientWidth;
+        var clientHeight = document.body.clientHeight;
+
+        var ratioW = clientWidth / globalVars.width;
+        var ratioH =  clientHeight / globalVars.height;
+
+        collie.Renderer.resize(globalVars.width * ratioW, globalVars.height * ratioH, true, true, false);
+        collie.Renderer.refresh();
+    }
+}*/
+(function(){
+    if (screen.width != config.statc.width || screen.height != config.statc.height) {
+
+        var clientWidth = document.body.clientWidth;
+        var clientHeight = document.body.clientHeight;
+
+        var ratioW = clientWidth / globalVars.width;
+        var ratioH =  clientHeight / globalVars.height;
+
+        collie.Renderer.resize(globalVars.width * ratioW, globalVars.height * ratioH, true, true, false);
+        collie.Renderer.refresh();
+    }
+})();
